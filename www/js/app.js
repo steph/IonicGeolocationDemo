455fb2f45aedd9df525b658b1c2dd2ede1dfdea7
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-10-02 12:17:38 +0100

    pulled in required code

--- www/js/app.js
+++ www/js/app.js
@@ -24,6 +24,37 @@ app.controller('MapController', ['$scope', '$cordovaGeolocation', function ($sco
 
   function onDeviceReady() {
 
+    $ionicLoading.show({
+      template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+    });
+
+    var posOptions = {
+      enableHighAccuracy: true,
+      timeout: 20000,
+      maximumAge: 0
+    };
+    $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+
+      var myLatlng = new google.maps.LatLng(lat, long);
+
+      var mapOptions = {
+        center: myLatlng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+      $scope.map = map;
+      $ionicLoading.hide();
+
+    }, function (err) {
+      $ionicLoading.hide();
+      console.log(err);
+    });
+
   }
 
 }]);
